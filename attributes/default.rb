eed8719e139528d4c498d54afc518abea6b1fdba
Ryan Cragun <ryan@rightscale.com>
2014-03-07 12:58:03 -0800

    Fauxhai 1.1.1 (which ChefSpec 2 uses and which we need to use to stub Chef 10.10 RightScale cookbooks) doesn't have an interfaces network key.  This commit works around that by setting a key in the node.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
+# 'Software'), to deal in the Software without restriction, including
 # without limitation the rights to use, copy, modify, merge, publish,
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
@@ -14,7 +14,7 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
@@ -26,7 +26,7 @@ node.default['cloud']['provider'] = 'vagrant'
 node.default['cloud']['private_ips'] = []
 node.default['cloud']['public_ips'] = []
 
-node.default['rightscale']['instance_uuid'] = "UUID"
+node.default['rightscale']['instance_uuid'] = 'UUID'
 node.default['rightscale']['servers']['sketchy']['hostname'] = 'localhost'
 
 # If it isn't set, set it
@@ -34,11 +34,17 @@ node.default['sys_firewall']['enabled'] = 'disabled'
 # If it is set, overwrite it
 node.normal['sys_firewall']['enabled'] = 'disabled'
 
-node.default['sys']['swap_size'] = "0.5"
-node.default['sys']['swap_file'] = "/mnt/ephemeral/swapfile"
+node.default['sys']['swap_size'] = '0.5'
+node.default['sys']['swap_file'] = '/mnt/ephemeral/swapfile'
 
 private_ips = []
 public_ips = []
+
+# Fauxhai 1.1.1 (which ChefSpec 2 uses) doesn't have an interfaces network key
+unless node['network'].key?('interfaces')
+  node.set['network']['interfaces'] = {}
+  node.set['network']['interfaces']['eth0'] = node['network']['eth0']
+end
 
 node['network']['interfaces'].each do |iface|
   iface[1]['addresses'].each do |addr|
@@ -46,12 +52,12 @@ node['network']['interfaces'].each do |iface|
     details = addr[1]
     if details['family'] == 'inet'
       case ip
-        when /^10|172|192\./
-          private_ips << ip
-        when "127.0.0.1"
-          # Intentionally don't do anything
-        else
-          public_ips << ip
+      when /^10|172|192\./
+        private_ips << ip
+      when '127.0.0.1'
+        # Intentionally don't do anything
+      else
+        public_ips << ip
       end
     end
   end
